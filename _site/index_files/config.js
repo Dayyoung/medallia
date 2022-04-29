0bc005094b64e32c8891253b090b407461a506db
Dayyoung you <dryudryu@squarenet.co.kr>
2022-04-29 14:36:42 +0900

    Start medallia

---
+++ _site/index_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C0JFBHSP76SVVJ0V28IG").setAdvancedMatchingAvailableProperties({"email":false,"phone_number":false,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C0JFBHSP76SVVJ0V28IG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C0JFBHSP76SVVJ0V28IG").setPixelInfo({status: 0, name: "TikTik Web Pixel", advertiserID: "6927687099135098882", setupMode: 0, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("medallia.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(false);
